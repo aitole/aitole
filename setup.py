4e51c1ba0f7a5650986953c21e675c94190f31df
aitole <93071769+aitole@users.noreply.github.com>
2022-09-01 12:49:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi2.py",
+    version = "0.1",
+    py_modules = ['agi2'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
